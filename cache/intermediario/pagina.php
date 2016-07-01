7ee252b537726eeca59c521ed8698eb3dba77455
Valdinei Reis <valdineimtz@gmail.com>
2016-07-01 18:02:13 -0300

    cache intermediario

---
+++ cache/intermediario/pagina.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Pagina</title>
+<head>
+<body>
+
+<div style="background-color:#ccc;width:300px;padding:5px 10px;">
+	<h2><?php echo rand(0, 9999) ?></h2>
+	<form>
+		Nome: <input type="text" name="nome" />
+		<br>
+		Senha: <input type="password" name="password" />
+	</form>
+</div>
+
+</body>
+</html>
